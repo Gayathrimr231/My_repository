042dd61937c54a140aa558e2ca596f4c582557ab
Gayathrimr231 <86797051+Gayathrimr231@users.noreply.github.com>
2021-07-16 18:37:36 +0530

    Add files via upload

---
+++ datatypes1.js
@@ -0,0 +1,14 @@
+//Datatypes using Any
+var empId = 225;
+var empName = " Gayathri ";
+var empDept = " Developer ";
+console.log(empId);
+console.log(empName);
+console.log(empDept);
+var empId2 = 304;
+var empName2 = " Rakshitha";
+var empDept2 = " Doctor ";
+var stmt = "Name:" + empName + " working in IT Department:" + empDept + " RegNo is :" + empId;
+var stmt2 = "Name:" + empName2 + " working in Medical Department:" + empDept2 + " RegNo is :" + empId2;
+console.log(stmt);
+console.log(stmt2);
---
+++ datatypes1.ts
@@ -0,0 +1,18 @@
+//Datatypes using Any
+var empId : any=225;
+var empName : string = " Gayathri ";
+var empDept : string = " Developer ";
+
+console.log(empId);
+console.log(empName);
+console.log(empDept);
+
+var empId2 : any = 304;
+var empName2 : string = " Rakshitha";
+var empDept2 : string = " Doctor ";
+
+var stmt: any = "Name:"+empName+" working in IT Department:"+empDept+" RegNo is :"+empId;
+var stmt2: any = "Name:"+empName2+" working in Medical Department:"+empDept2+" RegNo is :"+empId2;
+
+console.log(stmt);
+console.log(stmt2);
