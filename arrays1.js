d319f9892fefe75d24f02fe967761e22b5410d6b
Gayathrimr231 <86797051+Gayathrimr231@users.noreply.github.com>
2021-07-16 18:36:05 +0530

    Add files via upload

---
+++ arrays1.js
@@ -0,0 +1,13 @@
+//array function using :
+var arr_person;
+arr_person = [["Nivetha", 22, false],
+    ["Rakshitha", 23, true],
+    ["Deekshitha", 21, true]];
+console.log(arr_person);
+console.log(arr_person[2][0]);
+//using tuples
+var employee = [1, "Madhu"];
+employee[0];
+employee[1];
+employee.push(2, "Gayathri");
+console.log(employee);
---
+++ arrays1.ts
@@ -0,0 +1,14 @@
+//array function using :
+var arr_person: [ string,number,boolean][];
+arr_person=[[ `Nivetha`,22,false],
+              [`Rakshitha`,23,true],
+                [`Deekshitha`,21,true]];
+console.log(arr_person);
+console.log(arr_person[2][0]);
+
+//using tuples
+var employee : [number , string,]=[1, "Madhu"];
+employee[0];
+employee[1];
+employee.push(2, "Gayathri")
+console.log(employee);
